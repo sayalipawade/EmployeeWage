3dd01310950b33b41e335e59bd68346980a2ea9a
sayalipawade <sayalipawade05@gmail.com>
2020-03-20 01:33:07 +0530

    Added:Part time Employee and wages

--- EmpWage.sh
+++ EmpWage.sh
@@ -3,11 +3,16 @@ echo "Welcome to Employee Wage Computation"
 
 #constant
 EMP_PRESENT=1
+PART_TIME=2
 EMP_RATE_PER_HOUR=20
-attendence=$((RANDOM%2))
+attendence=$((RANDOM%3))
 if [[ $EMP_PRESENT -eq $attendence ]]
 then
 	empHours=8
+	EmpSalary=$(($empHours*$EMP_RATE_PER_HOUR))
+elif [[ $PART_TIME -eq $attendence ]]
+then
+	empHours=4
 	EmpSalary=$(($empHours*$EMP_RATE_PER_HOUR))
 else
 	empHours=0
