bca2812e6e08d343930b58a3965032c1d39104ea
sayalipawade <sayalipawade05@gmail.com>
2020-03-19 23:24:21 +0530

    Added:Checking whether employee is present or absent

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
+
+#constant
+EMP_PRESENT=1
+attendence=$((RANDOM%2))
+if [[ $EMP_PRESENT -eq $attendence ]]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
+
