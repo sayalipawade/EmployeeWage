83ca076299bc0a1ce032e2b66d04e02629c79f64
sayalipawade <sayalipawade05@gmail.com>
2020-03-20 11:20:31 +0530

    Added:Calculating wages for month

--- EmpWage.sh
+++ EmpWage.sh
@@ -6,23 +6,27 @@ EMP_PRESENT=1
 ISPART_TIME=2
 EMP_ABSENT=0
 EMP_RATE_PER_HOUR=20
+NUM_OF_WORKING_DAYS=20
 
 #variables
 empSalary=0
-attendence=$((RANDOM%3))
-case $attendence in
+
+for (( day=1;day<=$NUM_OF_WORKING_DAYS;day++))
+do
+	attendence=$((RANDOM%3))
+	case $attendence in
 		$EMP_PRESENT)
 				empHours=8
-				empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
 				;;
 		$ISPART_TIME)
 				empHours=4
-				empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
 				;;
 		$EMP_ABSENT)
 				empHours=0
-				empSalary=0
 				;;
-esac
-echo $empSalary
+	esac
+empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
+totalSalary=$(($totalSalary+$empSalary))
+done
+echo $totalSalary
 
