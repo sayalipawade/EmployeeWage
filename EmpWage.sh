b53a72511fd300fa00ee97faa932a67c30a879ef
sayalipawade <sayalipawade05@gmail.com>
2020-03-20 10:55:32 +0530

    Added:Calculating employee wage using case statement

--- EmpWage.sh
+++ EmpWage.sh
@@ -3,19 +3,26 @@ echo "Welcome to Employee Wage Computation"
 
 #constant
 EMP_PRESENT=1
-PART_TIME=2
+ISPART_TIME=2
+EMP_ABSENT=0
 EMP_RATE_PER_HOUR=20
+
+#variables
+empSalary=0
 attendence=$((RANDOM%3))
-if [[ $EMP_PRESENT -eq $attendence ]]
-then
-	empHours=8
-	EmpSalary=$(($empHours*$EMP_RATE_PER_HOUR))
-elif [[ $PART_TIME -eq $attendence ]]
-then
-	empHours=4
-	EmpSalary=$(($empHours*$EMP_RATE_PER_HOUR))
-else
-	empHours=0
-	EmpSalary=0
-fi
+case $attendence in
+			$EMP_PRESENT)
+						empHours=8
+						empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
+						;;
+			$ISPART_TIME)
+						empHours=4
+						empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
+						;;
+			$EMP_ABSENT)
+						empHours=0
+						empSalary=0
+						;;
+esac
+echo $empSalary
 
