058ce4c57fcf54bdb3b4b9a26dafdd0141682d1d
sayalipawade <sayalipawade05@gmail.com>
2020-03-23 14:06:59 +0530

    Refactor:Define empHours variable

--- EmpWage.sh
+++ EmpWage.sh
@@ -5,17 +5,21 @@ echo "Welcome to Employee Wage Computation"
 EMP_PRESENT=1
 PART_TIME=2
 EMP_RATE_PER_HOUR=20
+
+#variables
+empSalary=0
+
 attendence=$((RANDOM%3))
 if [[ $EMP_PRESENT -eq $attendence ]]
 then
 	empHours=8
-	EmpSalary=$(($empHours*$EMP_RATE_PER_HOUR))
+	empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
 elif [[ $PART_TIME -eq $attendence ]]
 then
 	empHours=4
-	EmpSalary=$(($empHours*$EMP_RATE_PER_HOUR))
+	empSalary=$(($empHours*$EMP_RATE_PER_HOUR))
 else
 	empHours=0
-	EmpSalary=0
+	empSalary=0
 fi
 
